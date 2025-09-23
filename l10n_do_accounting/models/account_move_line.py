9b06f1a2d4427c1bcb33dad240b6d7d07ca92751
ArobyR <ar.rosario001@gmail.com>
2025-09-23 12:21:31 -0400

    [FIX] l10n_do_accounting: external_id of taxes

--- l10n_do_accounting/models/account_move_line.py
+++ l10n_do_accounting/models/account_move_line.py
@@ -22,7 +22,7 @@ class AccountMoveLine(models.Model):
             if line.move_id.is_ecf_invoice:
                 line_itbis_taxes = line.tax_ids.filtered(
                     lambda t: t.tax_group_id
-                    == self.env.ref("account.%s_group_itbis" % line.company_id.id)
+                    == self.env.ref("account.%s_tax_group_itbis" % line.company_id.id)
                 )
                 price_unit = line.price_unit
                 if line.discount:
@@ -38,8 +38,8 @@ class AccountMoveLine(models.Model):
 
     def _get_l10n_do_line_amounts(self):
         group_itbis = self.env.ref(
-            "account.%s_group_itbis" % self.company_id.id)
-        group_isr = self.env.ref("account.%s_group_isr" % self.company_id.id)
+            "account.%s_tax_group_itbis" % self.company_id.id)
+        group_isr = self.env.ref("account.%s_tax_group_isr" % self.company_id.id)
 
         tax_lines = self.filtered(
             lambda x: x.tax_group_id.id
